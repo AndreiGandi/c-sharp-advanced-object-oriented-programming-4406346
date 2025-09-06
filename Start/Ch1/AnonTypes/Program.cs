95864489ee3d778109c268892569577e0d69c9e8
AndreiGdi <gandilaandrei@gmail.com>
2025-09-07 02:24:20 +0300

    Chapter1 - done

--- Start/Ch1/AnonTypes/Program.cs
+++ Start/Ch1/AnonTypes/Program.cs
@@ -1,13 +1,28 @@
-﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
+﻿ // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Creating and using Anonymous types
 
 // TODO: Anonymous types can be declared using "new" and { }, similar to JS
-
+var myobj = new
+{
+    Name = "John Doe", 
+    Age = 25, 
+    Address = new 
+    {
+        Street = "123 Main St", 
+        City = "Anytown", 
+        State = "NY"
+    } // Nested anonymous type
+};
+// anonymous type override ToString() method by default to display property names and values
+Console.WriteLine(myobj.ToString()); 
 
 // Anonymous types are read-only and cannot be modified
 // myobj.Name = "Jane Doe"; # will cause an error
 
 // TODO: To change a value, use non-destructive mutation and "with" clause
-
+var myobj2 = myobj with { Name = "Jane Doe" };
+Console.WriteLine(myobj2.ToString());
 
 // TODO: You can check to see if an anonymous object contains a property
+Console.WriteLine(myobj.GetType().GetProperty("Name") != null); // True
+Console.WriteLine(myobj.GetType().GetProperty("Phone") != null); // False
--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,14 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle rect1 = new Rectangle(10, 20);
+Rectangle rect2 = new Rectangle(5);
+
+Console.WriteLine(rect1.GetArea());
+Console.WriteLine(rect2.GetArea());
 
 
 // TODO: Change the values of width and height
+rect1.width = 15;
+rect1.height = 25;
+Console.WriteLine(rect1.GetArea());
